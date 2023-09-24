53e2fdcd734fb5a309a93acc6fc50072aaaa3080
Valya Tureyskaya <turleo@outlook.com>
2023-09-24 17:54:27 +1000

    🐛 Now big voice messages splitted by 10 seconds as required by wit.ai

--- modules/voicy.py
+++ modules/voicy.py
@@ -8,14 +8,18 @@ import json.decoder
 import logging
 from aiogram import Bot, Router
 from aiogram.types import Message
-from os import getenv
+from os import getenv, listdir
 from tempfile import TemporaryDirectory
 from subprocess import Popen, PIPE
 
 router = Router()
 log = logging.getLogger(__name__)
-log.setLevel(logging.ERROR)
-
+headers = {
+        "Authorization": "Bearer " + getenv("WITAI_TOKEN", ""),
+        "Content-Type": "audio/wav",
+        "Connection": "close"
+    }
+session = aiohttp.ClientSession(headers=headers)
 
 @router.message(lambda m: m.voice)
 async def voice_message_handler(message: Message) -> None:
@@ -40,34 +44,39 @@ async def file_decoder(file_id: str | None, mime: str | None, bot: Bot) -> str:
     directory = TemporaryDirectory()
     file_path = directory.name + f"/{file_id}.{mime.split('/')[1]}"
     await bot.download(file=file_id, destination=file_path)
-    wav = telegram_file_to_wav(file_path)
+    telegram_file_to_wav(file_path, directory.name)
     log.debug(file_path)
-    with open('out', 'wb+') as f:
-        f.write(wav)
-    headers = {
-        "Authorization": "Bearer " + getenv("WITAI_TOKEN", ""),
-        "Content-Type": "audio/wav",
-        "Connection": "close"
-    }
-    async with aiohttp.ClientSession(headers=headers) as session:
+    transcribed_text = ""
+    for i in listdir(directory.name):
+        if not i.endswith(".wav"):
+            continue
+        log.debug(f"Transcriing {i}")
+        transcribed_text += await file_transcriber(f"{directory.name}/{i}")
+            
+    return transcribed_text
+
+
+async def file_transcriber(file: str) -> str:
+    with open(file, 'rb') as f:
         async with session.post(
                 "https://api.wit.ai/speech",
                 headers=headers,
-                data=wav
+                data=f.read()
                 ) as resp:
             chunks = [chunk[0] async for chunk in resp.content.iter_chunks()]
             try:
                 out = json.loads(chunks[-1].decode())
+                return out["text"]
             except Exception:
                 log.error(chunks[-1].decode())
-                return ""
-            log.info(out)
-            return out["text"]
+                return "...😢..."
 
 
-def telegram_file_to_wav(path: str) -> bytes:
-    process = Popen(["ffmpeg", "-i",path, "-f", "wav", "-"], stdout=PIPE, stderr=PIPE)
+def telegram_file_to_wav(file_path: str, dir_path: str) -> None:
+    process = Popen([
+        "ffmpeg", "-i",file_path, 
+        "-segment_time", "00:00:10", "-f", "segment",
+        f"{dir_path}/%03d.wav"], stdout=PIPE, stderr=PIPE)
     out, err = process.communicate()
     log.debug(err)
-    return out
 
