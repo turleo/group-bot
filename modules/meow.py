5ef537a9515b01d27c3ebcdbf5baa69d4f476f62
Valya Tureyskaya <turleo@outlook.com>
2024-02-07 19:37:50 +1000

    🐛 meow regex bacame more accurate

--- modules/meow.py
+++ modules/meow.py
@@ -3,7 +3,7 @@ import re
 from aiogram import Router, types
 
 router = Router()
-meow_finder = re.compile(r'([Мм]([яувкрЯъУКВРЪ]?){0,}[\ \?\)\!]{0,}){1,}')
+meow_finder = re.compile(r'^([Мм]([яувкрЯъУКВРЪ]){1,}){1,}')
 
 @router.message(lambda m: meow_finder.search(m.text))
 async def meow(message: types.Message):
