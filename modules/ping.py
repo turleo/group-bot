088a08395ca5ef302a7b5a26b1c934a9559e86cf
Valya Tureyskaya <turleo@outlook.com>
2024-02-09 17:36:01 +1000

    🐛 Now ping returns commit that was the latest on start

--- modules/ping.py
+++ modules/ping.py
@@ -6,14 +6,14 @@ from aiogram import Router, types
 from aiogram.filters.command import Command
 
 log = logging.getLogger(__name__)
+version_bytes = subprocess.run(["git", "log", "-1", "--pretty=format:`%h` %s"],
+                         stdout=subprocess.PIPE).stdout
+version = version_bytes.decode("utf8")
 
 router = Router()
 
 @router.message(Command('ping'))
 async def pong(message: types.Message) -> None:
-    version_bytes = subprocess.run(["git", "log", "-1", "--pretty=format:`%h` %s"],
-                             stdout=subprocess.PIPE).stdout
-    version = version_bytes.decode("utf8")
     await message.reply(f"🏓 Pong\\!\n🏃‍♀️ Running `{uname()}`\n📜 Commit {version}")
     log.debug(f"{message.from_user.full_name} pinged")
 
