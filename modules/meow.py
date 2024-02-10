86fd82b51069bc37d3bc0f0dd06925948e622e71
Valya Tureyskaya <turleo@outlook.com>
2024-02-10 15:25:06 +1000

    🐛 Now meow handler not crashes if message have no text

--- modules/meow.py
+++ modules/meow.py
@@ -5,7 +5,7 @@ from aiogram import Router, types
 router = Router()
 meow_finder = re.compile(r'^([Мм]([яувкрЯъУКВРЪ]){1,}){1,}')
 
-@router.message(lambda m: meow_finder.search(m.text))
+@router.message(lambda m: m.text and meow_finder.search(m.text))
 async def meow(message: types.Message):
     await message.reply("мяу")
 
