bf8409637b4ad795a2c0c803bedc094f16274ba5
Valya Tureyskaya <me@turleo.me>
2024-08-27 19:42:24 +0300

    🐛 Fixed module indexing glob

--- src/dispatcher/dispatcher.ts
+++ src/dispatcher/dispatcher.ts
@@ -1,10 +1,10 @@
-import { Glob } from "bun";
+import { Glob } from "bun"
 import type { Dispatcher } from '@mtcute/dispatcher'
 import type { Api } from '@/api/types'
 
 export const importModules = async (dp: Dispatcher, api: Api) => {
   const pathes = (process.env.MODULES_PATH ?? './modules').split(';')
-  const glob = new Glob('**.ts')
+  const glob = new Glob('**/*.ts')
   for (const path of pathes) {
     
     for await (const file of glob.scan({ cwd: path, absolute: true })) {
